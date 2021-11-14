512b84f240a09736c8630e608778b783dafc89a8
mattéo <fauchonmatteo@gmail.com>
2021-11-14 21:56:35 +0100

    Initial Commit

---
+++ ShopIn/ShopInApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopInApp.swift
+//  ShopIn
+//
+//  Created by Matteo on 14/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShopInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
